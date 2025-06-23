da138e707a8870a12fbcac60f52d535084d9c577
mvbac <mv.bachaev@gmail.com>
2025-06-23 23:17:14 +0700

    уязвимость

--- src/main/java/org/example/todo/repository/TaskRepository.java
+++ src/main/java/org/example/todo/repository/TaskRepository.java
@@ -1,14 +1,31 @@
 package org.example.todo.repository;
 
+import com.sun.jdi.connect.spi.Connection;
 import org.example.todo.model.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
     List<TaskEntity> findAllByOrderByDeadlineAsc();
     List<TaskEntity> findAllByOrderByCreatedAtDesc();
+    List<TaskEntity> findTasksByTitle(String title) {
+        List<TaskEntity> tasks = new ArrayList<>();
+        try (Connection conn = dataSource.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery("SELECT * FROM tasks WHERE title = '" + title + "'")) {
+            while (rs.next()) {
 
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return tasks;
+    }
 }
